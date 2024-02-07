8b5d20d71f266f3a944642dee084a758d0d68172
Jonathan Memoli <jonomemoli3@gmail.com>
2024-02-07 17:09:23 -0500

    Update info

--- src/components/AboutMe/aboutMe.js
+++ src/components/AboutMe/aboutMe.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './aboutMe.css'
-import portrait from '../../assets/portrait_cropped-min.jpg'
+import portrait from '../../assets/portrait_dec23.jpg'
 
 
 const AboutMe = () => {
@@ -11,7 +11,7 @@ const AboutMe = () => {
                     <div className="aboutMeTitle">
                         <h1>About Me</h1>
                     </div>
-                    <p className="aboutMeText">I am currently in my third year at Stevens Institute of pursuing a Bachelors of Engineering in Software Engineering. I am eager to apply what I have learned as well as develop new skills in challenging engineering roles.
+                    <p className="aboutMeText">I am currently in my third year at Stevens Institute of Technology pursuing a Bachelors of Engineering in Software Engineering. I am eager to apply what I have learned as well as develop new skills in challenging engineering roles.
                         This past summer I worked with <a class="inlineLink" href="https://www.stevens.edu/profile/ealomar" target="_blank">Professor AlOmar</a> on the AntiCopyPaster tool for Intellij.<br/>
                         My programming skills include Java, Python, HTML/CSS, Javascript, C++, React.js, and SQL.<br/>
                         A few of my other interests besides programming include playing chess, skiing, and exercising!</p>
--- src/components/IntroPage/header.js
+++ src/components/IntroPage/header.js
@@ -10,13 +10,13 @@ const Intro = () => {
                     <span className="hello">Hello</span>
                     <span className="introText">I'm <span className="introName">Jonathan</span><br/>
                     Software Engineer</span>
-                    <p className="description">I'm currently an undergraduate Student at Stevens Institute of Technology with both frontend and backend experience.<br/>
+                    <p className="description">I'm currently an undergraduate Student at Stevens Institute of Technology with both frontend and backend development experience.<br/>
                     Connect with me here:</p>
                     <div class="socialIcons">
                         <ul class="socialIcons-list">
                             <li><a class="icon" href={resume} target="_blank"><ion-icon name="newspaper-outline"></ion-icon></a></li>
                             <li><a class="icon" href="https://github.com/JonMike8" target="_blank"><ion-icon name="logo-github"></ion-icon></a></li>
-                            <li><a class="icon" href="www.linkedin.com/in/jonathan-m-memoli" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                            <li><a class="icon" href="https://www.linkedin.com/in/jonathan-m-memoli/" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a></li>
                         </ul>
                     </div>
             </div>
